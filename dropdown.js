ad46d140d334b913413dc34ee89e86bbd488a542
gkowash <gkowash@gmail.com>
2022-06-15 17:02:00 +0200

    testing dropdown menu

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,14 @@
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+
+/*
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -5,11 +16,6 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
@@ -25,3 +31,4 @@ for (i = 0; i < coll.length; i++) {
 
   });
 }
+*/
