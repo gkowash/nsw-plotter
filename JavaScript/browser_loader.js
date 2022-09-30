46e5fb0b5e1312bcedc2e31603b89b00e37eaf4e
gkowash <gkowash@gmail.com>
2022-09-29 18:13:54 -0700

    Nav bar and file tree are now loaded with JQuery from files generated on EOS. browser_loader.js modified to allow drag/drop for plotting files with JSROOT. make_filetree.py generates html for file tree.

--- JavaScript/browser_loader.js
+++ JavaScript/browser_loader.js
@@ -1,11 +1,30 @@
-const load_file = function() {
+function load_file() {
   h.openRootFile(this.dataset.file);
   console.log("Opened file", this.dataset.file)
 }
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("file", ev.target["data-file"]);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("file");
+  load_file(data);
+}
+
 var file_links = document.getElementsByClassName("file");
 var i;
-
 for (i=0; i < file_links.length; i++) {
   file_links[i].addEventListener("click", load_file);
+  file_links[i].setAtribute("draggable", true);
+  file_links[i].setAtribute("ondragstart", drag(event));
 }
+
+var browser = document.getElementById("simpleGUI");
+browser.setAtribute("ondrop", drop(event));
+browser.setAtribute("ondragover", allowDrop(event));
---
+++ make_filetree.py
@@ -0,0 +1,64 @@
+# Script for automatically creating directory tree for stgc plotting application
+# Creates html file listing directories, which is then inserted into index.html via JQuery
+# Must be positioned in monitor_server folder (or adjust paths accordingly)
+# Probably better to avoid JQuery and do with PHP instead, if possible
+
+import os
+
+paths = []
+
+for root, dirs, files in os.walk('../calibrations/stg'):
+    for name in dirs:
+        #paths.append(os.path.join(root, name))  #Why did I include the root if I just remove it later?
+        paths.append(root + '/' + name)
+        print(paths[-1])
+        print("")
+
+
+# Currently paths look like ../calibrations/stg/STG-EA-S[##]/THRCalib/run[########]
+#paths_split = list(map(lambda p: p.split('/')[3:], paths))
+paths_split = list(map(lambda p: p.split('/'), paths))
+sectors = [path[-1] for path in paths_split if len(path)==4]  # len(path)==4 excludes subpaths of the "STG-EA-S[##]" directories
+
+runs_per_sector = {}
+for sector in sectors:
+    runs_per_sector[sector] = [path[-1] for path in paths_split if sector in path and len(path)==6]  # len(path)==6 selects only the "run[########]" folders
+
+
+print(runs_per_sector)
+
+html = '\t\t<ul id="file-tree">\n'
+
+for sector in sectors:
+    html += '\t'*3 + f'<li><span class="caret">{sector.split("-")[-1]}</span>\n'
+    html += '\t'*4 + '<ul class="nested">\n'
+
+    for run in runs_per_sector[sector]:
+        rootfile = None
+        for root, dirs, files in os.walk('../calibrations/stg/'+sector+'/THRCalib/'+run):
+            for name in files:
+                # Currently only using summary_plots.root
+                # outputcanvas.root and [...]_pdo_plots.root are also present
+                if 'summary_plots.root' in name:
+                    rootfile = name
+                    print('Root file: ', rootfile)
+                    break
+        if rootfile != None:
+            html += '\t'*5 + f'<li><a href="#" class="file" data-file="/stg/{sector}/THRCalib/{run}/{rootfile}">{run}</a></li>\n'
+        else:
+            html += '\t'*5 + f'<li><a href="#">{run} [nd]</a></li>\n'
+    html += '\t'*4 + '</ul>\n'
+    html += '\t'*3 + '</li>\n'
+
+html += '\t\t</ul>\n'
+html += '\t\t<script src="JavaScript/dropdown.js"></script>'
+# ^dropdown.js needs to be executed afer JQuery finishes loads for the menu to function properly
+# Solution: stop using JQuery
+
+
+print('\n\n\n')
+print(html)
+
+file = open('filetree.html', 'w')
+file.writelines(html)
+file.close()
